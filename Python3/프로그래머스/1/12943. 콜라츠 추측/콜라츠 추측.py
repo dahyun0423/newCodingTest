f10a29a9933d562d0bcd46a97034550462b1e39d
dahyun(다현) <128135741+dahyun0423@users.noreply.github.com>
2025-10-01 22:40:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    count = 0
+    while num != 1:
+        if num % 2 == 0 : 
+            num //= 2
+            count +=1
+        else:
+            num = (num * 3) + 1
+            count+=1
+        if count > 500 and num != 1:
+            return -1
+    return count
